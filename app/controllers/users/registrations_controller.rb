2a6777d8998065e1b348531fa4a948e2a248e251
rileymanda <rileymanda69@gmail.com>
2024-11-23 22:50:44 +0200

    Registration controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,21 @@
+# app/controllers/users/registrations_controller.rb
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  # POST /users
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      render json: { message: "User created successfully", user: resource }, status: :created
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+  end
+end
