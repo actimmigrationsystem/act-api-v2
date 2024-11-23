acdf2c127468aeeadfa14fe8764147addc6db9d5
rileymanda <rileymanda69@gmail.com>
2024-11-23 18:23:38 +0200

    Appointments|Profiles|ActiveStorageTables|Enquiries|Schema|Routes

---
+++ db/migrate/20241123161607_create_enquiries.rb
@@ -0,0 +1,23 @@
+class CreateEnquiries < ActiveRecord::Migration[7.2]
+  def change
+    create_table :enquiries do |t|
+      t.string :name
+      t.string :surname
+      t.string :phonenumber
+      t.string :email
+      t.string :gender
+      t.date :dob
+      t.string :marital_status
+      t.string :residential_address
+      t.string :immigration_status
+      t.date :entry_date
+      t.string :passport_number
+      t.string :reference_number
+      t.string :document_upload
+      t.string :service_type
+      t.text :elaborate
+
+      t.timestamps
+    end
+  end
+end
