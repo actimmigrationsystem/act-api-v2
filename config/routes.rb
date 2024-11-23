61391ed243f8b337cc5c41fa1d8db507c4d0ddd0
rileymanda <rileymanda69@gmail.com>
2024-11-23 22:52:02 +0200

    Devise custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
   resources :enquiries
   resources :appointments
   resources :profiles
