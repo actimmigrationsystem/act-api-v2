d9ad93fe56a0d750cb26c59ce07b314d2c86cf52
rileymanda <rileymanda69@gmail.com>
2024-12-07 10:59:00 +0200

    Implemented user_id to appointments and enquiries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_07_083028) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_07_085616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_07_083028) do
     t.string "appointment_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
   create_table "enquiries", force: :cascade do |t|
@@ -44,6 +46,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_07_083028) do
     t.string "immigration_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_enquiries_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -72,5 +76,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_07_083028) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "users"
+  add_foreign_key "enquiries", "users"
   add_foreign_key "profiles", "users"
 end
