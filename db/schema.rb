3d6acb0b2c8653921910d2e371e7a7054ba3ad9a
rileymanda <rileymanda69@gmail.com>
2025-01-08 00:05:30 +0200

    Precompile fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,73 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_241_207_085_616) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_07_085616) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'appointments', force: :cascade do |t|
-    t.string 'name'
-    t.string 'surname'
-    t.string 'phonenumber'
-    t.string 'email'
-    t.string 'service_type'
-    t.string 'venue'
-    t.datetime 'appointment_date'
-    t.string 'appointment_type'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_appointments_on_user_id'
+  create_table "appointments", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "phonenumber"
+    t.string "email"
+    t.string "service_type"
+    t.string "venue"
+    t.datetime "appointment_date"
+    t.string "appointment_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
-  create_table 'enquiries', force: :cascade do |t|
-    t.string 'name'
-    t.string 'surname'
-    t.string 'phonenumber'
-    t.string 'email'
-    t.string 'gender'
-    t.date 'dob'
-    t.string 'marital_status'
-    t.string 'residential_address'
-    t.date 'entry_date'
-    t.string 'passport_number'
-    t.string 'reference_number'
-    t.string 'service_type'
-    t.text 'elaborate'
-    t.string 'immigration_status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id', null: false
-    t.index ['user_id'], name: 'index_enquiries_on_user_id'
+  create_table "enquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "phonenumber"
+    t.string "email"
+    t.string "gender"
+    t.date "dob"
+    t.string "marital_status"
+    t.string "residential_address"
+    t.date "entry_date"
+    t.string "passport_number"
+    t.string "reference_number"
+    t.string "service_type"
+    t.text "elaborate"
+    t.string "immigration_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_enquiries_on_user_id"
   end
 
-  create_table 'profiles', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'phone_number'
-    t.text 'address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_profiles_on_user_id'
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'role'
-    t.string 'auth_token'
-    t.index ['auth_token'], name: 'index_users_on_auth_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role"
+    t.string "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'appointments', 'users'
-  add_foreign_key 'enquiries', 'users'
-  add_foreign_key 'profiles', 'users'
+  add_foreign_key "appointments", "users"
+  add_foreign_key "enquiries", "users"
+  add_foreign_key "profiles", "users"
 end
